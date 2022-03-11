e63d4cdc1ea2802e4371142a0ecb177f5bc0ab90
MTahaOzsahin <ozsahinm16@itu.edu.tr>
2022-03-11 07:27:28 +0300

    Tutorial follow day2

--- Assets/GameFolders/Scripts/Controllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Controllers/PlayerController.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace UdemyprojectTutorialBerk1.Controllers
+{
+    public class PlayerController : MonoBehaviour
     {
-        
+        Rigidbody2D _rigidbody2D;
+        bool _isLeftMouseClicked;
+
+       [SerializeField] float jumpForce;
+
+        //public float JumpForce            Encapsulation ornegi
+        //{
+        //    get
+        //    {
+        //        return jumpForce;
+        //    }
+        //    set
+        //    {
+        //        if (value <1f || value > 1000f)
+        //        {
+        //            jumpForce = 500f;
+        //        }
+        //        else
+        //        {
+        //            jumpForce = value;
+        //        }
+        //    }
+                
+        //}
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                _isLeftMouseClicked = true;
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (_isLeftMouseClicked)
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                _rigidbody2D.AddForce(Vector2.up * jumpForce);
+                _isLeftMouseClicked = false;
+            }
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            GameManager.Instance.RestartGame();
+        }
     }
 }
+
